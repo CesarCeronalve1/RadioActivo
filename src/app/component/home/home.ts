74afa2135c6e901dece88190714943447c001f47
cesarceronalve1 <comcesar1@gmail.com>
2025-07-06 12:40:06 -0600

    se adjusto el sidebar

--- src/app/component/home/home.ts
+++ src/app/component/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SideBar } from "../side-bar/side-bar";
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule],
+  imports: [RouterModule, SideBar],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
---
+++ src/app/component/reproductor/reproductor.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-reproductor',
+  imports: [RouterModule],
+  templateUrl: './reproductor.html',
+  styleUrl: './reproductor.css'
+})
+export class Reproductor {
+
+}
